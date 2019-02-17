9d1fee0b7865efe4004e02148d6fafeba679a38c
Cory Kim <corykim0829@gmail.com>
2019-02-17 17:57:05 +0900

    feat calculator pad

---
+++ public/calculatorFunctions.js
@@ -0,0 +1,15 @@
+function add(var a, var b){
+    return a+b;
+}
+
+function multiply(var a, var b){
+    return a*b;
+}
+
+function divide(var a, var b){
+    return a/b;
+}
+
+function subtract(var a, var b){
+    return a-b;
+}
