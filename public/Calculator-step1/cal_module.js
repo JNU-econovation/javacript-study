fd77d53ab11d151c7efe900d3ca90eb992559c6a
Hiro Lee <mtoc227@gmail.com>
2019-02-18 20:13:36 +0900

    feat: calculator

---
+++ public/Calculator-step1/cal_module.js
@@ -0,0 +1,29 @@
+function plus(n1, n2) {
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+    return n1+n2;
+}
+
+function minus(n1, n2) {
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+    return n1-n2;
+}
+
+function multi(n1, n2) {
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+    return n1*n2;
+}
+
+function divide(n1, n2) {
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+    return parseFloat(n1/n2);
+}
+
+function mod(n1, n2) {
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+    return n1%n2;
+}
