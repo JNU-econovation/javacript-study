fb8dcdccf8206982e9236576f23c228704a0ceb0
Cory Kim <corykim0829@gmail.com>
2019-02-24 17:39:09 +0900

    update: caclculator button ui for step2

---
+++ public/calculator-step2/calculator-step2.js
@@ -0,0 +1,89 @@
+function inputPad(i){
+    document.getElementById("inputBar").value += i;
+}
+
+function clearAllBars(){
+    document.getElementById("inputBar").value = "";
+    document.getElementById("resultBar").value = "";
+}
+
+function equal(){
+    var tempInput = document.getElementById("inputBar").value;
+    var input = tempInput.replace(" ","");
+    
+    if(checkIsException(input)){
+        errors();
+        return;
+    }
+    
+    var ResultObj = checkRegex(input);
+    var result = doMath(ResultObj.numA, ResultObj.numB, ResultObj.calSym);
+    
+    if(result == null){
+        alert("0으로 나눌 수 없습니다.");
+        return;
+    }
+    document.getElementById("resultBar").value = result;
+}
+
+function errors(){
+    alert("잘못된 형식입니다.");
+    clearAllBars();
+}
+
+function checkIsException(input){
+    if(input == null) {return true;}
+    var splitedBySym = input.split(/[+*/-]/);
+    if(splitedBySym[0] == "" || splitedBySym[splitedBySym.length-1] == "") {return true;}
+    if(splitedBySym.length >= 3) {return true;}
+    if(input.match(/[a-z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i) != null) {return true;}
+}
+
+function add(leftNum, rightNum){
+    return parseInt(leftNum)+parseInt(rightNum);
+}
+
+function multiply(leftNum, rightNum){
+    return parseInt(leftNum)*parseInt(rightNum);
+}
+
+function divide(leftNum, rightNum){
+    return parseInt(leftNum)/parseInt(rightNum);
+}
+
+function subtract(leftNum, rightNum){
+    return parseInt(leftNum)-parseInt(rightNum);
+}
+
+function checkRegex(input){
+    var patternNums = /(\d+)/g;
+    var patternCalSym = /[+*/-]/;
+    var numA = input.match(patternNums)[0];
+    var numB = input.match(patternNums)[1];
+    var calSym = input.match(patternCalSym)[0];
+    
+    var ResultObj = {
+        'numA' : numA,
+        'numB' : numB,
+        'calSym' : calSym
+    }
+    return ResultObj;
+}
+
+function doMath(leftNum, rightNum, calSym){
+    switch(calSym){
+        case '+':
+            return add(leftNum, rightNum);
+            break;
+        case '*':
+            return multiply(leftNum, rightNum);
+            break;
+        case '-':
+            return subtract(leftNum, rightNum);
+            break;
+        case '/':
+            if(numB == 0){return null;}
+            return divide(leftNum, rightNum);
+            break;
+    }
+}
