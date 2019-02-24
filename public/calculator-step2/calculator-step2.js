8e2aee9d11966fc779b3335b84b481157ed56171
Cory Kim <corykim0829@gmail.com>
2019-02-25 01:46:05 +0900

    feat: calculator with Queue

--- public/calculator-step2/calculator-step2.js
+++ public/calculator-step2/calculator-step2.js
@@ -1,15 +1,19 @@
-var isLaterNumberInput = false;
-var isLaterNumberInputStart = false;
-var CalculatorData = {
-    'formerNumber': null,
-    'laterNumber': null,
-    'symbol': null
-}
+var isNumberInputDone = false;
+var isNumberInputStart = false;
+var calculatorQueue = [];
+
+/*
+var queue = [];
+queue.push(2);         // queue is now [2]
+queue.push(5);         // queue is now [2, 5]
+var i = queue.shift(); // queue is now [5]
+alert(i);              // displays 2
+*/
 
 function clickNumber(number){
     var displayBar = document.getElementById("displayBar");
     
-    if(!isLaterNumberInput){
+    if(!isNumberInputDone){
         if(displayBar.value != 0){
             displayBar.value += number;    
             return;
@@ -18,83 +22,60 @@ function clickNumber(number){
         return;
     }
     
-    if(isLaterNumberInput){
-        if(isLaterNumberInputStart){
+    if(isNumberInputDone){
+        if(isNumberInputStart){
             displayBar.value += number;
             return;
         }
         displayBar.value = number;
-        isLaterNumberInputStart = true;
+        isNumberInputStart = true;
         return;
     }
 }
 
 function clickSymbol(symbol){
     var displayBar = document.getElementById("displayBar");
+    calculatorQueue.push(displayBar.value);
+    console.log("clickSymbol : " + calculatorQueue);
     
-    if(CalculatorData.formerNumber != null){
-        equal();
-    }
-    
-    CalculatorData.formerNumber = displayBar.value;
-    CalculatorData.symbol = symbol;
-    
-    isLaterNumberInput = true;
-    isLaterNumberInputStart = false;
-}
-
-function initCalculatorData(){
-    CalculatorData.formerNumber = null;
-    CalculatorData.laterNumber = null;
-    CalculatorData.symbol = null;
-}
-
-function equal(){
-    if(CalculatorData.laterNumber == null){
+    // Queue
+    if(calculatorQueue.length == 3){
+        console.log("Bfter Do Math : " + calculatorQueue);
+        calculatorQueue.push(doMath());
+        displayBar.value = calculatorQueue[0];
+        console.log("After Do Math : " + calculatorQueue);
+        calculatorQueue.push(symbol);
+        isNumberInputDone = true;
+        isNumberInputStart = false;
         return;
     }
     
-    CalculatorData.laterNumber = displayBar.value;
-    isLaterNumberInput = false;
-    isLaterNumberInputStart = false;
-    document.getElementById("displayBar").value = doMath(CalculatorData);
-    initCalculatorData();
+    calculatorQueue.push(symbol);
+    
+    isNumberInputDone = true;
+    isNumberInputStart = false;
+}
+
+function equal(){
+    var displayBar = document.getElementById("displayBar");
+    calculatorQueue.push(displayBar.value);
+    
+    if(calculatorQueue.length == 1){
+        var tempNumber = calculatorQueue.shift();
+        displayBar.value = tempNumber;
+        return;
+    }
+    
+    isNumberInputDone = false;
+    isNumberInputStart = false;
+    displayBar.value = doMath();
 }
 
 function clearDisplay(){
+    calculatorQueue = [];
     document.getElementById("displayBar").value = "0";
-}
-
-function equal(){
-    var tempInput = document.getElementById("inputBar").value;
-    var input = tempInput.replace(" ","");
-    
-    if(checkIsException(input)){
-        errors();
-        return;
-    }
-    
-    var ResultObj = checkRegex(input);
-    var result = doMath(ResultObj.numA, ResultObj.numB, ResultObj.calSym);
-    
-    if(result == null){
-        alert("0으로 나눌 수 없습니다.");
-        return;
-    }
-    document.getElementById("displayBar").value = result;
-}
-
-function errors(){
-    alert("잘못된 형식입니다.");
-    clearDisplay();
-}
-
-function checkIsException(input){
-    if(input == null) {return true;}
-    var splitedBySym = input.split(/[+*/-]/);
-    if(splitedBySym[0] == "" || splitedBySym[splitedBySym.length-1] == "") {return true;}
-    if(splitedBySym.length >= 3) {return true;}
-    if(input.match(/[a-z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i) != null) {return true;}
+    isNumberInputDone = false;
+    isNumberInputStart = false;
 }
 
 function add(leftNum, rightNum){
@@ -113,31 +94,20 @@ function subtract(leftNum, rightNum){
     return parseFloat(leftNum)-parseFloat(rightNum);
 }
 
-function checkRegex(input){
-    var patternNums = /(\d+)/g;
-    var patternCalSym = /[+*/-]/;
-    var numA = input.match(patternNums)[0];
-    var numB = input.match(patternNums)[1];
-    var calSym = input.match(patternCalSym)[0];
+function doMath(){
+    var formerNumber = calculatorQueue.shift();
+    var symbol = calculatorQueue.shift();
+    var laterNumber = calculatorQueue.shift();
     
-    var ResultObj = {
-        'numA' : numA,
-        'numB' : numB,
-        'calSym' : calSym
-    }
-    return ResultObj;
-}
-
-function doMath(CalculatorData){
-    switch(CalculatorData.symbol){
+    switch(symbol){
         case '+':
-            return add(CalculatorData.formerNumber, CalculatorData.laterNumber);
+            return add(formerNumber, laterNumber);
         case '*':
-            return multiply(CalculatorData.formerNumber, CalculatorData.laterNumber);
+            return multiply(formerNumber, laterNumber);
         case '-':
-            return subtract(CalculatorData.formerNumber, CalculatorData.laterNumber);
+            return subtract(formerNumber, laterNumber);
         case '/':
-            if(CalculatorData.laterNumber == 0){return 'ERROR';}
-            return divide(CalculatorData.formerNumber, CalculatorData.laterNumber);
+            if(laterNumber == 0){return 'ERROR';}
+            return divide(formerNumber, laterNumber);
     }
 }
