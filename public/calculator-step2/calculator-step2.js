0bf52aa1dad1ca825dc11081b19adc8900b42ef5
Cory Kim <corykim0829@gmail.com>
2019-03-01 14:35:36 +0900

    refactor : indent, lines

--- public/calculator-step2/calculator-step2.js
+++ public/calculator-step2/calculator-step2.js
@@ -11,13 +11,12 @@ function initCalculator(){
 
 function inputNumberByKey(e){
 	const key = document.querySelector(`td[data-key="${e.keyCode}"]`);
-	if(key == null) return;
-	console.log(key);
+	if(!key) return;
 	key.onclick();
 }
 
 function clickNumber(number){
-    var displayBar = document.getElementById("displayBar");
+	var displayBar = document.getElementById("displayBar");
 
 	if(number == '.'){
 		if(isDotUsed) {
@@ -39,61 +38,60 @@ function clickNumber(number){
 }
 
 function clickSymbol(symbol){
-    var displayBar = document.getElementById("displayBar");
-    calculatorQueue.push(displayBar.value);
+	var displayBar = document.getElementById("displayBar");
+	calculatorQueue.push(displayBar.value);
 
-    if(calculatorQueue.length == 3){
-        // should check its composite is num-sym-num
-        calculatorQueue.push(doMath());
-        displayBar.value = calculatorQueue[0];
-        calculatorQueue.push(symbol);
+	if(calculatorQueue.length == 3){
+		calculatorQueue.push(doMath());
+		displayBar.value = calculatorQueue[0];
+		calculatorQueue.push(symbol);
 		inputStatement = DONE;
 		isDotUsed = false;
-        return;
-    }
-    calculatorQueue.push(symbol);
+		return;
+	}
+	calculatorQueue.push(symbol);
 	inputStatement = DONE;
 	isDotUsed = false;
 }
 
 function equal(){
-    var displayBar = document.getElementById("displayBar");
-    calculatorQueue.push(displayBar.value);
+	var displayBar = document.getElementById("displayBar");
+	calculatorQueue.push(displayBar.value);
 
-    if(calculatorQueue.length == 1){
-        calculatorQueue.shift();
-        return;
-    }
-    inputStatement = DONE;
-    displayBar.value = doMath();
+	if(calculatorQueue.length == 1){
+		calculatorQueue.shift();
+		return;
+	}
+	inputStatement = DONE;
+	displayBar.value = doMath();
 }
 
 function clearDisplay(){
-    calculatorQueue = [];
-    document.getElementById("displayBar").value = "0";
-    inputStatement = DONE;
+	calculatorQueue = [];
+	document.getElementById("displayBar").value = "0";
+	inputStatement = DONE;
 	isDotUsed = false;
 }
 
 function calculate(symbol){
-    var CalculationFuncs = {
-        '+' : function(former, later){return former+later;},
-        '-' : function(former, later){return former-later;},
-        '*' : function(former, later){return former*later;},
-        '/' : function(former, later){
-            if(later == 0){
-                return "ERROR";
-            }
-            return former/later;
-        },
-    }
-    return CalculationFuncs[symbol];
+	var CalculationFuncs = {
+		'+' : function(former, later){return former+later;},
+		'-' : function(former, later){return former-later;},
+		'*' : function(former, later){return former*later;},
+		'/' : function(former, later){
+			if(later == 0){
+				return "ERROR";
+			}
+			return former/later;
+		},
+	}
+	return CalculationFuncs[symbol];
 }
 
 function doMath(){
-    var formerNumber = parseFloat(calculatorQueue.shift());
-    var symbol = calculatorQueue.shift();
-    var laterNumber = parseFloat(calculatorQueue.shift());
+	var formerNumber = parseFloat(calculatorQueue.shift());
+	var symbol = calculatorQueue.shift();
+	var laterNumber = parseFloat(calculatorQueue.shift());
 
-    return calculate(symbol)(formerNumber, laterNumber);
+	return calculate(symbol)(formerNumber, laterNumber);
 }
