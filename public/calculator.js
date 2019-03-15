24ec641922b9711d6c5f66ac82468d6055d2e8f8
forbid403 <forbid403@naver.com>
2019-03-15 22:50:35 +0900

    [bird] calculator step2 구현 완료

--- public/calculator.js
+++ public/calculator.js
@@ -1,61 +1,102 @@
-
+var queue = [];
+var typing = false;
+var dotUsed = false;
+var queueSize = 3;
+var numExp = /[0-9]/;
+var operExp = /[+/*-]/;
+    
 function getInput() {
     return document.getElementById("input");
 }
 
-function run() {
-    var regExp = /[0-9]/;
-    var operExp = /[+/*-]/;
-    window.addEventListener('keydown', function(element){
+function initInput() {
+    getInput().value = 0;
+    typing = true;
+    dotUsed = false;
+    queue = [];
+}
 
-        if(element.keyCode == 13) {
-            calculate();
-        }
+window.addEventListener('keydown', function(element){
+    if(element.keyCode == 13) { submitClicked(element.key); }
+    if(element.keyCode == 8){ clearClicked(element.key); }
+    if(element.key.match(numExp)){ numberClicked(element.key); }
+    if(element.key.match(operExp)){ operClicked(element.key); }
+    if(element.keyCode == 190){ dotClicked(element.key); }
+});
 
-        if(element.keyCode == 8){
-            clearAll();
-        }
+function clearClicked(element) {
+    clearAll();
+    initInput();
+}
 
-        if(element.key.match(regExp)){
-            getInput().value += element.key;
-        }
+function submitClicked(element) {
+    queue.push(getInput().value);
+    calculate();
+    typing = true;
+    dotUsed = false;
+    queue = [];
+}
 
-        if(element.key.match(operExp)){
-            
-        }
+function numberClicked(element) {
+    console.log(element);
+    if(typing) clearAll();
+    typing = false;
+    getInput().value += element;
+}
 
-    });
+function operClicked(element) {
+    typing = true;
+    dotUsed = false;
+    queue.push(getInput().value);
+    if(queue.length == queueSize) {
+        calculate();
+    }
+    queue.push(element);
+}
+
+function dotClicked(element) {
+    if(dotUsed) {
+        return;
+    }
+    typing = false;
+    dotUsed = true;
+    getInput().value += element;
+}
+
+function input(element) {
+    getInput().value += element.innerHTML;
+}
+
+function clearAll() {
+    getInput().value = '';
 }
 
 function calculate() {
-    var result = operator(getInput());
+    var result = operator();
+    console.log(result)
     getInput().value = result;
 }
 
-function check(splited) {
-    if(splited.length > 2){
-        return false;
-    }
-
-    return true;
+function dealError() {
+    alert("error");
+    initInput();
 }
 
+function operator() {
+    var first = queue.pop();
+    var operator = queue.pop();
+    var second = queue.pop();
+    console.log(second, operator, first);
 
-function operator(operator) {
-    var input = operator.value;
-    var regExp = /[+*/-]/;
+    var result = selectOperator(operator)(parseFloat(second), parseFloat(first));
     
-    var splited = String(input).split(regExp);
-    var operator = String(input).match(regExp);
-    var result = selectOperator(operator)(parseFloat(splited[0]), parseFloat(splited[1]));
-
-    if(check(result) && isFinite(result)){
+    if(isFinite(result)){
+        queue.push(result);
         return result;
     }
     
     dealError();
     return "";
-
 }
 
 function selectOperator(operator) {
@@ -66,17 +107,4 @@ function selectOperator(operator) {
         '*' : function (first, second) {return first * second}
     }
     return calculation[operator];
-}
-
-
-function dealError() {
-    alert("error");
-}
-
-function input(element) {
-    getInput().value += element.innerHTML;
-}
-
-function clearAll() {
-    getInput().value = '';
 }
