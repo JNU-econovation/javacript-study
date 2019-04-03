8c3a3e2a58decf9d84863a8ea2a942d130a9ebbf
forbid403 <forbid403@naver.com>
2019-04-03 16:49:42 +0900

    fix : 동기식 변경, 함수 축소

--- day4/fileReading.js
+++ day4/fileReading.js
@@ -1,23 +1,12 @@
-const http = require('http');
-const server = http.createServer();
 const fs = require('fs');
-const host = '192.168.0.2';
-const port = 3000;
 
-server.listen(port, host, 50000, function() {
-    console.log("web server start : " + port + ',' + host);
-    const array = fs.readFileSync('info.json');
-    let parsed = JSON.parse(array);
-    
-    for(i in parsed.info) {
+const array = function(){
+    let file = fs.readFileSync('info.json', 'utf8');
+    let parsed = JSON.parse(file);
+      
+    for(let i in parsed.info) {
         console.log(parsed.info[i].name);
     }
-});
+}
 
-server.on('connection', function(socket) {
-    console.log("client is connected");
-});
-
-server.on('request', function(req, res) {
-    console.log("client requested : ");
-});
+array();
--- day4/server.js
+++ day4/server.js
@@ -3,14 +3,18 @@ const net = require('net');
 
 var server = net.createServer(function(client){
     console.log('Client connected');
-    client.write('Hello');
+    
     client.on('data', function(data){
         console.log(data.toString());
+        client.write(data);
     });
+
     client.on('end',function(){
         console.log('Client disconnected');
     });
+
 });
+
 server.listen(port, function(){
     console.log('Server listening for connections');
 });
