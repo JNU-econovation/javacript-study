a33e89617827c6addc2d77f53a4c3881ed7444fe
GiPyoo <a01054394111@gmail.com>
2019-04-03 14:13:58 +0900

    feat : 공백구분

--- file.io.js
+++ file.io.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
-const readline = require('readline');
-const filename = './public/data/member.txt';
+const readline = require('readline')
 
 function readByOneLineInFile(filename) {
     const stream = fs.createReadStream(filename);
     const reader = readline.createInterface(stream, process.stdout,null);
 
+    let count = 0;
+
     reader.on('line', function (line) {
+        count += 1;
+        const tokens = line.split(' ');
+
+        if (tokens != undefined && tokens.length > 0) {
+            console.log('#' + count + ":" + tokens[0])
+        }
     });
 
     reader.on('close', function (line) {
-        console.log('\n모두 읽음');
+        console.log('모두 읽음');
     });
 }
 
+const filename = './public/data/member.txt';
 readByOneLineInFile(filename);
