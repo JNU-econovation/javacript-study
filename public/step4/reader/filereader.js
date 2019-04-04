c442a5f094e56ffe54eac2557a7346580af4f514
Park Hyunseo <hyeonseo9669@hanmail.net>
2019-04-04 17:10:26 +0000

    Modifiy filereader.js

--- public/step4/reader/filereader.js
+++ public/step4/reader/filereader.js
@@ -2,11 +2,11 @@ const readline = require('readline');
 const fs = require('fs');
 const fileName = 'data.txt';
 
-let rl = readline.createInterface({
+const readlineInterface = readline.createInterface({
     input: fs.createReadStream(fileName)
 });
     
-rl.on('line', function(line){
+readlineInterface.on('line', function(line){
     const rows = line.split(' ');
     console.log(rows[0]);
 });
