7d676227a222416ea13356b6f7b6aa9817ec4f4e
Park Hyunseo <hyeonseo9669@hanmail.net>
2019-04-04 17:00:38 +0000

    Modify files

--- public/calculate-step2/static/js/utils.js
+++
@@ -1,9 +0,0 @@
-const isNumber = (string) => {
-  string += ''; // 문자열로 변환
-  string = string.replace(/^\s*|\s*$/g, ''); // 좌우 공백 제거
-  
-  if (string == '' || isNaN(string)) 
-    return false;
-  
-  return true;
-}
