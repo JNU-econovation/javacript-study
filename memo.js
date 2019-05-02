fd19dd841b8dae309bc833df81090b0f2a5fe13c
GiPyoo <a01054394111@gmail.com>
2019-05-02 11:31:39 +0900

    save memo :fire: : feat : image processing

--- memo.js
+++ memo.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 const static = require('serve-static');
+const multer = require("multer")
 
 const app = express();
 
@@ -13,47 +14,66 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/public', static(path.join(__dirname, 'public')));
+app.use('/uploads', static(path.join(__dirname, 'uploads')))
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, './uploads/');
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    }),
+}).single('userfile');
 
 app.post("/save", function (req, res) {
     try {
-        var name = req.body.name;
-        var date = req.body.date;
-        var content = req.body.content;
-        console.log(name);
-        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' })
-        res.write(`
-        <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="/public/stylesheet/momo.css">
-        <title>Document</title>
-        </head>
-        <form method="GET" action="/" id="re-form">
-        <table class="header-table">
-        <thead>
-            <th class="table-head">나의 메모</th>
-        </thead>
-        <tbody class="table-body">
-            <tr>
-                <td>${name},${date},${content}메모가 저장되었습니다.</td>
-            </tr>
-        </tbody>
-        </table>
-        <table class="footer-table">
-            <tbody>
-                <tr>
-                    <td id="re-layor"><button id="redirect" form = "re-form" type="submit" >다시작성</button></td>
-                </tr>
-            </tbody>
-        </table>
-        </form>
-    `);
-        res.end();
+        upload(req, res, function (err) {
+            var name = req.body.name;
+            var date = req.body.date;
+            var content = req.body.content;
+            var filename = req.file.filename;
+            console.log(filename);
+            res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+            res.write(`
+            <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" href="/public/stylesheet/momo.css">
+            <title>Document</title>
+            </head>
+            <form method="GET" action="/" id="re-form">
+            <table class="header-table">
+                <thead>
+                    <th class="table-head">나의 메모</th>
+                </thead>
+                <tbody class="table-body">
+                    <tr>
+                        <td>${name},${date},${content}메모가 저장되었습니다.</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="upload-display">
+                            <div class="upload-thumb-wrap"><img class="upload-thumb" src="/uploads/${filename}"></div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="footer-table">
+                <tbody>
+                    <tr><td id="re-layor"><button id="redirect" form = "re-form" type="submit" >다시작성</button></td></tr>
+                </tbody>
+            </table>
+            </form>
+            `);
+            res.end();
+        });
+
     } catch (err) {
         console.dir(err.stack);
-        
-		res.writeHead(400, {'Content-Type':'text/html;charset=utf8'});
-		res.write('<div><p>메모 저장 시 에러 발생</p></div>');
-		res.end();
+
+        res.writeHead(400, { 'Content-Type': 'text/html;charset=utf8' });
+        res.write('<div><p>메모 저장 시 에러 발생</p></div>');
+        res.end();
     }
 
 });
